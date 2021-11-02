cec86aa80fcc942dad14a19329d1776d90d765a0
ANDRA-FLORINA-MARIA SĂSĂRMAN <andra.sasarman@stud.ubbcluj.ro>
2021-11-02 22:37:13 +0200

    Add project files.

---
+++ Sasarman_Andra_Lab5M/Models/Expenses.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sasarman_Andra_Lab5M.Models
+{
+    public class Expenses
+    {
+        public int Id { get; set; }
+
+        public decimal Amount { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+        public string Secret { get; set; }
+    }
+}
